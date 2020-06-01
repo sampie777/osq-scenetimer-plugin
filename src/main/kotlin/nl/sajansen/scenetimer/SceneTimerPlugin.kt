74ab9ae218390af1ed4c579ea05c7e40b1c13f07
S. Jansen <sa.jansen1@gmail.com>
2020-06-01 12:41:01 +0200

    Compatible with OBS Scene Queue v2.5.0

--- src/main/kotlin/nl/sajansen/scenetimer/SceneTimerPlugin.kt
+++ src/main/kotlin/nl/sajansen/scenetimer/SceneTimerPlugin.kt
@@ -4,14 +4,17 @@ import gui.utils.getMainFrameComponent
 import plugins.PluginLoader
 import plugins.common.DetailPanelBasePlugin
 import nl.sajansen.scenetimer.config.ConfigWindow
+import java.util.logging.Logger
 import javax.swing.*
 
 @Suppress("unused")
 class SceneTimerPlugin : DetailPanelBasePlugin {
+    private val logger = Logger.getLogger(SceneTimerPlugin::class.java.name)
+
     override val name: String = "SceneTimerPlugin"
     override val description: String = "Plugin for displaying the current scene live time"
     override val version: String = PluginInfo.version
-    override val icon: Icon? = null
+    override val icon: Icon? = createImageIcon("/nl/sajansen/scenetimer/icon-14.png")
 
     override val tabName = "Scene Timer"
 
@@ -39,4 +42,14 @@ class SceneTimerPlugin : DetailPanelBasePlugin {
     }
 
     private fun getMainMenu(menu: JMenu) = (menu.popupMenu.invoker.parent as JPopupMenu).invoker
+
+    private fun createImageIcon(path: String): ImageIcon? {
+        val imgURL = SceneTimerPlugin::class.java.getResource(path)
+        if (imgURL != null) {
+            return ImageIcon(imgURL)
+        }
+
+        logger.severe("Couldn't find imageIcon: $path")
+        return null
+    }
 }
